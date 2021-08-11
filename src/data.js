81c67ce2afc7749e0df01102442553cfbb4ff8fc
linusidom <linusidom@gmail.com>
2021-08-11 18:44:17 +0700

    updated tours project youtube link

--- src/data.js
+++ src/data.js
@@ -46,7 +46,7 @@ const data = [
     {
         id: 5,
         netlifyLink:'https://spotskillz-tours-project.netlify.app/',
-        youtubeLink:'https://www.youtube.com/watch?v=KlPRXYNmW2E',
+        youtubeLink:'https://www.youtube.com/watch?v=Ah9musT74Nk',
         githubLink:'https://github.com/linusidom/coding101/tree/main/reactIntro/tours_project_redone',
         imageLink:'../assets/ToursProject.png',
         title: 'Tours Project',
